1d8e0ba5a85bd3f961b97d44d6c0a06b76f39de5
Yaroslav Berezhnev <93657405+YBerezhnev@users.noreply.github.com>
2022-01-28 20:11:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 28 18:55:57 2022
-
-@author: BerezhnevYM
-"""
 
 import setuptools
 
 setuptools.setup(
-    name='src',                    # package name
+    name='BayInV',                    # package name
     version='0.1',                          # version
     description='Implimentation of the Bayesian least square approach for velocity changes measurements',      # short description
     url='http://example.com',               # package URL
