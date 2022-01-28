0030d81c95e28745c39034e10d3b3e0cc613429b
Yaroslav Berezhnev <93657405+YBerezhnev@users.noreply.github.com>
2022-01-28 19:50:01 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 18:55:57 2022
+
+@author: BerezhnevYM
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='BayInV',                    # package name
+    version='0.1',                          # version
+    description='Implimentation of the Bayesian least square approach for velocity changes measurements',      # short description
+    url='http://example.com',               # package URL
+    install_requires=[ "numpy", "scipy", "numba", "matplotlib", "seaborn"],                    # list of packages this package depends
+                                            # on.
+    packages=setuptools.find_packages(),              # List of module names that installing
+    )
