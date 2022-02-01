f57e0f35d1564d814c549e5c173ade8e48330b9e
Yaroslav Berezhnev <93657405+YBerezhnev@users.noreply.github.com>
2022-02-01 16:30:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='BayInV',                    # package name
-    version='0.1',                          # version
+    version='0.01',                          # version
     description='Implimentation of the Bayesian least square approach for velocity changes measurements',      # short description
     url='https://github.com/YBerezhnev/BayInV',               # package URL
     install_requires=[ "numpy", "scipy", "numba", "matplotlib", "seaborn"],                    # list of packages this package depends
